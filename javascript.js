8e3de743f54798626291c31bd15a976c1d215fed
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-07-09 21:09:32 +0600

    all added

--- javascript.js
+++ javascript.js
@@ -11,3 +11,30 @@ const navTosggleFunc = function () {nav.classList.toggle('active');}
 
 navMenuBtn.addEventListener('click', navTosggleFunc);
 navCloseBtn.addEventListener('click', navTosggleFunc);
+
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+
+for (let i = 0; i < themeBtn.length; i++) {
+
+  themeBtn[i].addEventListener('click', function () {
+
+    // toggle `light-theme` & `dark-theme` class from `body`
+    // when clicked `theme-btn`
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for (let i = 0; i < themeBtn.length; i++) {
+
+      // When the `theme-btn` is clicked,
+      // it toggles classes between `light` & `dark` for all `theme-btn`.
+      themeBtn[i].classList.toggle('light');
+      themeBtn[i].classList.toggle('dark');
+
+    }
+
+  })
+
+}
